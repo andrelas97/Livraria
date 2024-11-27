e4dbf879cea1d0621573cbf0ec485305fa247f6b
andrelas97 <andrelas97@hotmail.com>
2024-11-27 15:43:38 -0300

    adcionei a footer e iniciei a parte do quero doar

--- src/Pages/QueroDoar/querodoar.jsx
+++ src/Pages/QueroDoar/querodoar.jsx
@@ -1,7 +1,22 @@
+import S from './querodoar.module.scss'
+import Livro from '../../assets/Vector.png'
+
 export default function LivrosDoados() {
     return (
-      <section>
-        <h2>EU SOU O COMPONENTE LIVROS DOADOS</h2>
+      <section className={S.principal}>
+        <section className={S.container}>
+          <h2>Por favor, preencha o formulário com suas informações e as informações do Livro</h2>
+          <form action="">
+            <div>
+              <img src={Livro} alt=""/>
+              <h3>Informações do Livro</h3>
+            </div>
+         <input type="text" placeholder='Titulo' />
+         <input type="text" placeholder='Categoria'/>
+         <input type="text" placeholder='Autor'/>
+         <input type="text" placeholder='Link da imagem'/>
+          </form>
+        </section>
       </section>
     );
   }
