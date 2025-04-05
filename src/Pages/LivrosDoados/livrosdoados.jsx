e84518d739ca59b4d873fd61460e187409608c61
andrelas97 <andrelas97@hotmail.com>
2025-04-04 21:44:30 -0300

    alterações

--- src/Pages/LivrosDoados/livrosdoados.jsx
+++ src/Pages/LivrosDoados/livrosdoados.jsx
@@ -1,13 +1,29 @@
 import S from './doados.module.scss'
-import Protagonista from '../../assets/book.png'
+import livro from '../../assets/book.png'
+import axios from 'axios'
+import { useState, useEffect } from 'react'
 
 export default function LivrosDoados() {
+  const [livros, setLivros] = useState([])
+
+  const getLivros = async () => {
+      try {
+          const response = await axios.get("https://teste-deploy-bkai.onrender.com/livros")
+          setLivros(response.data)
+      } catch (error) {
+          console.error("Erro ao buscar livros:", error)
+      }
+  }
+  useEffect(() => {
+      getLivros()
+  }, [])
+
     return (
       <section className={S.boxDoados}>
         <h2>Livros Doados</h2>
         <section className={S.boxCard}>
-          <article>
-            <img src={Protagonista} alt="" />
+        <article>
+            <img src={livro} alt="" />
             <h3>O Protagonista</h3>
             <p>Susanne Andrade</p>
             <p>Ficção</p>
@@ -16,4 +32,3 @@ export default function LivrosDoados() {
       </section>
     );
   }
-  
--- src/Pages/QueroDoar/querodoar.jsx
+++ src/Pages/QueroDoar/querodoar.jsx
@@ -1,7 +1,58 @@
 import S from './querodoar.module.scss'
 import Livro from '../../assets/Vector.png'
+import axios from 'axios'
+import {useState} from 'react'
 
-export default function LivrosDoados() {
+export default function Querodoar() {
+
+  const [titulo, setTitulo] = useState("")
+  const [categoria, setCategoria] = useState("")
+  const [autor, setAutor] = useState("")
+  const [imagem_url, setImagem_url] = useState("")
+
+
+  const enviarDados = async()=>{
+      
+      const urlApi = "https://teste-deploy-bkai.onrender.com/doar"
+
+    
+      const dadosEnviar = {
+          titulo,
+          categoria,
+          autor,
+          imagem_url
+      }
+
+      await axios.post(urlApi, dadosEnviar)
+
+      alert("Livro cadastrado!!") 
+
+    
+      setTitulo("")
+      setCategoria("")
+      setAutor("")
+      setImagem_url("")
+  }
+
+
+  const capturaTitulo = (e) =>{
+      setTitulo(e.target.value)
+      console.log(e.target.value)
+  }
+
+  const capturaCategoria = (e) =>{
+      setCategoria(e.target.value)
+  }
+
+  const capturaAutor = (e) =>{
+      setAutor(e.target.value)
+  }
+
+  const capturaImagem = (e) =>{
+      setImagem_url(e.target.value)
+  }
+
+
     return (
       <section className={S.principal}>
         <section className={S.container}>
